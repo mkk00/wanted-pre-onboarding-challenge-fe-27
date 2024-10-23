55b17b63a45af4fa1d43f88c256ad85dba1d83d8
mkk <mk1210k@gmail.com>
2024-10-24 00:49:46 +0900

    회원가입 페이지 기본 레이아웃 추가

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,5 +1,46 @@
+import {
+  AuthLayout,
+  AuthContainer,
+  FormWrapper,
+  InputRow,
+  Label,
+  Input,
+  Button,
+} from '@/pages/auth/Auth.styled'
+
+import { Title } from '@/styles/common.styled'
+
 function Signup() {
-  return <>회원가입 페이지</>
+  return (
+    <AuthLayout>
+      <AuthContainer>
+        <Title>회원가입</Title>
+        <FormWrapper>
+          <InputRow>
+            <Label htmlFor="email">이메일</Label>
+            <Input type="email" id="email" placeholder="이메일을 입력하세요" />
+          </InputRow>
+          <InputRow>
+            <Label htmlFor="password">비밀번호</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="비밀번호를 입력하세요"
+            />
+          </InputRow>
+          <InputRow>
+            <Label htmlFor="passwordConfirm">비밀번호 확인</Label>
+            <Input
+              type="password"
+              id="passwordConfirm"
+              placeholder="비밀번호를 다시 입력하세요"
+            />
+          </InputRow>
+          <Button type="submit">회원가입</Button>
+        </FormWrapper>
+      </AuthContainer>
+    </AuthLayout>
+  )
 }
 
 export default Signup
