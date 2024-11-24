225ff75e721bc330cfc5f193f2406e2af7319f76
mkk <mk1210k@gmail.com>
2024-11-24 17:44:34 +0900

    zustand persist 미들웨어 추가하여 유저 정보 상태 유지 적용
    - persist : 로컬 스토리지에 데이터 저장 후 새로고침 시마다 로컬 스토리지에서 해당 데이터를 가져옴
    - persist type error 해결 : https://cloverinks.medium.com/nextjs-zustand-typescript-type-conflict-with-persist-middleware-d72c2b3ec286

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,4 +1,5 @@
-import { create } from 'zustand'
+import { create, StateCreator } from 'zustand'
+import { persist, PersistOptions } from 'zustand/middleware'
 
 interface User {
   email: string
@@ -11,8 +12,20 @@ interface UserStore {
   logout: () => void
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  user: null,
-  login: (user: User) => set({ user }),
-  logout: () => set({ user: null }),
-}))
+type UserStorePersist = (
+  config: StateCreator<UserStore>,
+  options: PersistOptions<UserStore>,
+) => StateCreator<UserStore>
+
+export const useUserStore = create<UserStore>(
+  (persist as UserStorePersist)(
+    (set) => ({
+      user: null,
+      login: (user: User) => set({ user }),
+      logout: () => set({ user: null }),
+    }),
+    {
+      name: 'userInfo',
+    },
+  ),
+)
